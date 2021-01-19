d5dafe1d306a6570391aa8ef50be34bb2c5536c0
BrendowLincoln <linconlbrendow@gmail.com>
2021-01-18 22:12:46 -0300

    Criando a lista de receitas para cada categoria em especifica

--- lib/screens/categories_recipes_screen.dart
+++ lib/screens/categories_recipes_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:receita_mania/data/dummy_data.dart';
 import 'package:receita_mania/models/category_model.dart';
 
 class CategoriesRecipesScreen extends StatelessWidget {
@@ -11,13 +12,22 @@ class CategoriesRecipesScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final CategoryModel category = ModalRoute.of(context).settings.arguments as CategoryModel;
 
+    final categoryRecipes = DUMMY_RECIPES.where((recipe) {
+      return recipe.categories.contains(category.id);
+    }).toList();
+
     return Scaffold(
       appBar:  AppBar(
         title: Text('Receitas'),
         centerTitle: true,
       ),
       body: Center(
-        child: Text('Receitas por categorias ${category.id}'),
+        child: ListView.builder(
+          itemCount: categoryRecipes.length,
+          itemBuilder: (context, index) {
+            return Text(categoryRecipes[index].title);
+          },
+        ),
       ),
     );
   }

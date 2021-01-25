6b63dd735883c5266966eb7466a9cf472dc94aef
BrendowLincoln <linconlbrendow@gmail.com>
2021-01-25 19:29:10 -0300

    Elimando a pilha de telas

--- lib/screens/settings_screen.dart
+++ lib/screens/settings_screen.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:receita_mania/widgets/main_drawer.dart';
 
 class SettingsScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: Text('Configurações'), centerTitle: true,),
+      drawer: MainDrawer(),
       body: Center(
         child: Text('Settings Screen'),
       ),
